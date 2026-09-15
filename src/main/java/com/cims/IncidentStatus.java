876439a3c66c0a3fd01de9fe075ef7f06081c672
MatheshChandKV <matheshchand.25bai10318@vitbhopal.ac.in>
2026-09-15 17:46:38 +0530

    status identification for a incident

---
+++ src/main/java/com/cims/IncidentStatus.java
@@ -0,0 +1,8 @@
+package com.cims;
+public enum IncidentStatus {
+    REPORTED,
+    OPEN,
+    INVESTIGATING,
+    RESOLVED,
+    CLOSED
+}
---
+++ src/main/java/com/cims/IncidentType.java
@@ -0,0 +1,8 @@
+package com.cims;
+public enum IncidentType {
+    PHISHING,
+    MALWARE,
+    UNAUTHORIZED_ACCESS,
+    DATA_BREACH,
+    DENIAL_OF_SERVICE
+}
